cf317a5325fb1082ce9ab7bb491f02bbc264fca8
AnilNekuri <anilnekuri@gmail.com>
2016-10-27 18:26:35 +0530

    Display Tree

--- src/com/algorithmica/tree/TreeSize.java
+++ src/com/algorithmica/tree/TreeSize.java
@@ -12,7 +12,8 @@ public class TreeSize<T> {
 		TreeSize<Integer> ts = new TreeSize<Integer>();
 		ts.generateTree(Integer.parseInt(args[0]));
 		int sizeR = ts.sizeR(ts.root);
-		System.out.println(sizeR);
+		ts.displayTree();
+		System.out.println(sizeR);		
 		int sizeQ = ts.sizeQ(ts.root);
 		System.out.println(sizeQ);
 		
@@ -72,6 +73,37 @@ public class TreeSize<T> {
 			}
 			
 		}
+	}
+	
+	public void displayTree(){
+		printNode(root,0);
+	}
+	
+	public void printNode(TreeNode<T> node,int level){
+		
+		if(node == null) return;
+		String whiteSpace = "";
+		for(int i = 0;i<level;i++){
+			whiteSpace += "     ";
+		}
+		System.out.println(whiteSpace+"|__("+(node.data.toString().length() == 1 ? node.data.toString()+" " : node.data.toString())+")");
+		//System.out.print(whiteSpace+(node.data.toString().length() == 1 ? node.data.toString()+" " : node.data.toString())+"-->");
+		level = level+1;
+		if(node.left != null || node.right != null){
+			if(node.left != null)printNode(node.left,level);	
+			else printNull(level);
+			//System.out.println();
+			if(node.right != null)printNode(node.right,level);	
+			else printNull(level);
+		}		
+	}
+	
+	public void printNull(int level){
+		String whiteSpace = "";
+		for(int i = 0;i<level;i++){
+			whiteSpace += "     ";
+		}
+		System.out.println(whiteSpace+"|__null");
 	}
 	
 }
