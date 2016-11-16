a7f74cc5f90d6f4187ae862a037a8b16be545b19
Anil Nekuri <anilnekuri@gmail.com>
2016-11-16 08:05:04 +0530

    Ternary

---
+++ src/com/algorithmica/greedy/TernarySequence.java
@@ -0,0 +1,24 @@
+package com.algorithmica.greedy;
+
+import java.util.Arrays;
+
+public class TernarySequence {
+
+	static int n = 0;
+	
+	public static void main(String[] args) {
+		n = Integer.parseInt(args[0]);
+		tseq(new int[n], n);
+	}
+	
+	public static void tseq(int[] ter, int d) {
+		if (d <= 0) {
+			System.out.println(Arrays.toString(ter));
+			return;
+		}
+		for (int i = 0; i <= 2; i++) {
+			ter[n - d] = i;
+			tseq(ter, d-1);
+		}
+	}
+}
