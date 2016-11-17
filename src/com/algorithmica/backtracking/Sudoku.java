3dfa8f3098740298ac88c193d57023898cc1647c
Anil Nekuri <anilnekuri@gmail.com>
2016-11-17 22:50:49 +0530

    Sudoku done

--- src/com/algorithmica/backtracking/Sudoku.java
+++ src/com/algorithmica/backtracking/Sudoku.java
@@ -2,55 +2,37 @@ package com.algorithmica.backtracking;
 
 public class Sudoku {
 	
+	static int[][] indexOrder = new int[81][2]; 
+	
 	static int[][] sudoArr = new int[9][9];
 	
 	public static void main(String[] args) {
-		sudo1();	
+		sudo();		
 	}
 	
-	public static void sudo1(){
-		for(int is = 0,ix = 2; is < 9 && ix < 9; is += 3,ix += 3){
-			for(int js = 0, jx = 2; js < 9 && jx < 9;js += 3,jx += 3 ){
-				System.out.println("i -> ("+is+"-"+ix+") , j -> ("+js+"-"+jx+")");
-				fill3x3Block(is, ix, js, jx);
-			}
-			if(sudoArr[8][8] != 0){
-				display();
-				sudoArr[8][8] = 0;
-			}
-		}		
+	public static void sudo(){
+		createOrder();
+		fillSudo(0);
 	}
 	
-	private static void display() {
-		for(int i = 0; i < 9; i++){			
-			for(int j = 0; j < 9; j++){	
-				System.out.print(" "+sudoArr[i][j]);
-			}
+	public static void fillSudo(int i){
+		if(i == 81){
+			display();
 			System.out.println();
+			return;
+		}
+		for(int n = 1; n <= 9; n++){	
+			if(isValid2(i, indexOrder[i][0], indexOrder[i][1], n)){
+				sudoArr[indexOrder[i][0]][indexOrder[i][1]] = n;
+				fillSudo(i+1);
+			}			
 		}
 	}
-
-	public static void fill3x3Block(int is,int ix,int js,int jx){
-		int[] block = new int[9];
-		int index = 0;
-		for(int i = is; i <= ix; i++){
-			for(int j = js; j <= jx; j++){
-				for(int k = 1; k <= 9; k++){
-					if(isValid(block,i,j,k)){
-						block[index] = k;
-						sudoArr[i][j] = k;
-						index++;
-						break;
-					}
-				}
-			}
-		}
-	}
-
-	private static boolean isValid(int[] block, int i, int j,int k) {
+	
+	private static boolean isValid2(int oIndex, int i, int j,int k) {
 		boolean isValid = true;
-		for(int b = 0; b < block.length && block[b] != 0; b++){
-			if(k == block[b]){
+		for(int b = (oIndex/9)*9; b < oIndex; b++){
+			if(k == sudoArr[indexOrder[b][0]][indexOrder[b][1]]){
 				isValid = false;
 				break;
 			}
@@ -62,6 +44,33 @@ public class Sudoku {
 			isValid = false;
 		}
 		return isValid;
+	}
+	
+	
+	public static void createOrder(){
+		int index = 0;
+		for(int is = 0,ix = 2; is < 9 && ix < 9; is += 3,ix += 3){
+			for(int js = 0, jx = 2; js < 9 && jx < 9;js += 3,jx += 3 ){
+				for(int x = is; x <= ix; x++){
+					for(int y = js; y <= jx; y++){
+						int[] tmp = new int[2]; 
+						tmp[0] = x;
+						tmp[1] = y;
+						indexOrder[index] = tmp;
+						index++;
+					}
+				}
+			}			
+		}
+	}
+	
+	private static void display() {
+		for(int i = 0; i < 9; i++){			
+			for(int j = 0; j < 9; j++){	
+				System.out.print(" "+sudoArr[i][j]);
+			}
+			System.out.println();
+		}
 	}
 
 	private static boolean isNotExistInColumn(int k,int i, int j) {
