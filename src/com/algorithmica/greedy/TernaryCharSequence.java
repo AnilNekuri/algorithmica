0cb7d6c8b0d0a73a9f03a059d9d010bcac0223d1
Anil Nekuri <anilnekuri@gmail.com>
2016-11-17 07:12:11 +0530

    Ternary char sequence

---
+++ src/com/algorithmica/greedy/TernaryCharSequence.java
@@ -0,0 +1,27 @@
+package com.algorithmica.greedy;
+
+import java.util.Arrays;
+
+public class TernaryCharSequence {
+
+	static int nc = 0;
+	static int n  = 0;
+	
+	public static void main(String[] args) {
+		nc = Integer.parseInt(args[0]);
+		n  = Integer.parseInt(args[1]);
+		terSeq(new char[n], n);
+	}
+	
+	public static void terSeq(char[] arr,int d){
+		if(d == 0){
+			System.out.println(Arrays.toString(arr));
+			return;
+		}
+		char c = 'A';
+		for(int i = 0; i < nc;i++,c++){
+			arr[n-d] = c;
+			terSeq(arr, d-1);
+		}
+	}
+}
